b1c62c8590f5eac143e4345dd08e0cacefa98afd
matiasmartin <matiass.lean@gmail.com>
2024-08-24 16:43:04 +0200

    feat: define endpoints and add middleware for security

--- internal/adapters/in/http/routes.go
+++ internal/adapters/in/http/routes.go
@@ -1,10 +1,49 @@
 package http
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/proyectum/ms-user-profile/internal/adapters/in/http/api"
+	"github.com/proyectum/ms-user-profile/internal/adapters/in/http/security"
+	"net/http"
 )
 
+type profileRoutes struct{}
+
+func (p *profileRoutes) GetProfile(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{
+		"message": "not implemented yet",
+	})
+}
+
+func (p *profileRoutes) UpdateProfile(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{
+		"message": "not implemented yet",
+	})
+}
+
+func (p *profileRoutes) UpdateNotification(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{
+		"message": "not implemented yet",
+	})
+}
+
+func (p *profileRoutes) GetTypes(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{
+		"message": "not implemented yet",
+	})
+}
+
+func (p *profileRoutes) errorHandler(c *gin.Context, err error, status int) {
+
+}
+
 func RegisterRoutes(r *gin.Engine) {
-	fmt.Println("implement")
+	routes := profileRoutes{}
+	api.RegisterHandlersWithOptions(
+		r,
+		&routes,
+		api.GinServerOptions{
+			Middlewares:  []api.MiddlewareFunc{security.JwtMiddleware},
+			ErrorHandler: routes.errorHandler,
+		})
 }
